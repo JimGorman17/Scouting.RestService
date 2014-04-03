3745d1d58de39e9fda282b5d9b28e17064fb6c77
TKirby42 <jimgorman2004@yahoo.com>
2014-04-02 22:47:26 -0400

    Added two service methods for reporting.

--- Scouting.RestService/Api/CommentService.cs
+++ Scouting.RestService/Api/CommentService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using Scouting.DataLayer;
+using Scouting.DataLayer.Models;
 using ServiceStack;
 
 namespace Scouting.RestService.Api
@@ -48,6 +50,63 @@ namespace Scouting.RestService.Api
                 });
 
             return new HttpStatusResult(HttpStatusCode.OK);
+        }
+
+        [Route("/Comment/GetTotalsByTeam")]
+        public class CommentGetTotalsByTeamRequest
+        {
+        }
+
+        public class TeamCommentRow
+        {
+            public string Team { get; set; }
+            public int Count { get; set; }
+        }
+
+        public object Get(CommentGetTotalsByTeamRequest request)
+        {
+            Database db = new Database("localDB"); // TODO: Use IOC
+
+            var results = db.Query<TeamCommentRow>("SELECT T.Location + ' ' + T.Nickname AS [Team], COUNT(C.CommentID) AS [Count]" +
+                                        "FROM Comments C " +
+                                        "INNER JOIN Players P " +
+                                        "ON	(C.PlayerID = P.PlayerID) " +
+                                        "INNER JOIN Teams T " +
+                                        "ON	(P.Team = T.Abbreviation) " +
+                                        "GROUP BY T.Location, T.Nickname " +
+                                        "ORDER BY COUNT(C.CommentID) DESC");
+
+            return results;
+        }
+
+        [Route("/Comment/GetTotalsByUser")]
+        public class CommentGetTotalsByUserRequest
+        {
+            public int NumberOfUsers { get; set; }
+        }
+
+        public class CommentUserRow
+        {
+            public string Picture { get; set; }
+            public string DisplayName { get; set; }
+            public string FavoriteTeam { get; set; }
+            public int Count { get; set; }
+        }
+
+        public object Get(CommentGetTotalsByUserRequest request)
+        {
+            Database db = new Database("localDB"); // TODO: Use IOC
+
+            var results = db.Query<CommentUserRow>("SELECT U.Picture, U.DisplayName, T.Location + ' ' + T.Nickname AS [FavoriteTeam], COUNT(C.CommentID) AS [Count] " +
+                                                   "FROM Comments C " +
+                                                   "INNER JOIN Users U " +
+                                                   "ON	(C.GoogleID = U.GoogleID) " +
+                                                   "LEFT OUTER JOIN Teams T " +
+                                                   "ON (U.FavoriteTeamID = T.TeamID)" +
+                                                   "GROUP BY U.Picture, U.DisplayName, T.Location, T.Nickname " +
+                                                   "ORDER BY COUNT(C.CommentID) DESC");
+
+            return results.Take(request.NumberOfUsers);
         }
     }
 }
