0a5780cdaf369f4728873d2543d98bdeae892f31
TKirby42 <jimgorman2004@yahoo.com>
2014-04-13 14:21:41 -0400

    Added some formatting to the flagged comment.

--- Scouting.DataLayer/Models/FlaggedCommentForModerator.cs
+++ Scouting.DataLayer/Models/FlaggedCommentForModerator.cs
@@ -7,6 +7,8 @@ namespace Scouting.DataLayer.Models
     {
         public int CommentId { get; set; }
         public string Comment { get; set; }
+        public string PlayerName { get; set; }
+        public string TeamName { get; set; }
         public DateTimeOffset UpdateDate { get; set; }
         public string LastUpdated
         {
@@ -14,6 +16,10 @@ namespace Scouting.DataLayer.Models
             {
                 return DateTimeOffset.Now.Subtract(UpdateDate).ToReadableString(true);
             }
+        }
+        public string FormattedComment
+        {
+            get { return String.Format("{0}<br><b><small>{1}, {2}, {3}</small></b>", Comment.Trim(), PlayerName, TeamName, LastUpdated); }
         }
         public int NumberOfFlags { get; set; }
     }
--- Scouting.DataLayer/Repositories/FlaggedCommentRepository.cs
+++ Scouting.DataLayer/Repositories/FlaggedCommentRepository.cs
@@ -14,15 +14,19 @@ namespace Scouting.DataLayer.Repositories
         public List<FlaggedCommentForModerator> GetFlaggedCommentsForModerator()
         {
             var results = Database.Query<FlaggedCommentForModerator>(
-                "SELECT C.CommentID, C.Comment, COALESCE(C.UpdateDate, C.CreateDate) AS [UpdateDate], COUNT(C.CommentID) AS [NumberOfFlags] " +
+                "SELECT C.CommentID, C.Comment, RTRIM(LTRIM(P.FirstName + ' ' + P.LastName)) AS [PlayerName], RTRIM(LTRIM(T.Location + ' ' + T.Nickname)) AS [TeamName], COALESCE(C.UpdateDate, C.CreateDate) AS [UpdateDate], COUNT(C.CommentID) AS [NumberOfFlags] " +
                 "FROM FlaggedComments FC " +
                 "INNER JOIN Comments C " +
                 "ON	(FC.CommentID = C.CommentID) AND " +
                 "(C.Deleted = 0) " +
+                "INNER JOIN Players P " +
+                "ON (C.PlayerID = P.PlayerID) " +
+                "INNER JOIN Teams T " + 
+                "ON (P.Team = T.Abbreviation) " +
                 "INNER JOIN Users U " +
                 "ON	(FC.GoogleID = U.GoogleID) " +
                 "WHERE (FC.Handled = 0) " +
-                "GROUP BY C.CommentID, C.Comment, COALESCE(C.UpdateDate, C.CreateDate) " +
+                "GROUP BY C.CommentID, C.Comment, RTRIM(LTRIM(P.FirstName + ' ' + P.LastName)), RTRIM(LTRIM(T.Location + ' ' + T.Nickname)), COALESCE(C.UpdateDate, C.CreateDate) " +
                 "ORDER BY COUNT(C.CommentID) DESC, COALESCE(C.UpdateDate, C.CreateDate) DESC");
 
             return results.ToList();
